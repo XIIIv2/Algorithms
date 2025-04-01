efa69cd264f1601629d14b37dee83163c4f81191
xiii <kirill.popov@icloud.com>
2025-04-01 20:46:17 +0300

    Initital commit

---
+++ src/app/ArrayUtils.java
@@ -0,0 +1,52 @@
+package app;
+
+import java.util.Arrays;
+
+public class ArrayUtils {
+    public int[] mergeSort(int[] arrayA) {
+        if (arrayA == null) {
+            return null;
+        }
+
+        if (arrayA.length < 2) {
+            return arrayA;
+        }
+
+        int [] arrayB = new int[arrayA.length / 2];
+        System.arraycopy(arrayA, 0, arrayB, 0, arrayA.length / 2);
+
+        int [] arrayC = new int[arrayA.length - arrayA.length / 2];
+        System.arraycopy(arrayA, arrayA.length / 2, arrayC, 0, arrayA.length - arrayA.length / 2);
+
+        arrayB = mergeSort(arrayB);
+        arrayC = mergeSort(arrayC);
+
+        return mergeArray(arrayB, arrayC);
+    }
+
+    private static int[] mergeArray(int[] arrayA, int[] arrayB) {
+        int[] result = new int[arrayA.length + arrayB.length];
+        int i = 0;
+        int positionA = 0;
+        int positionB = 0;
+
+        while (positionA < arrayA.length && positionB < arrayB.length)
+            result[i++] = arrayA[positionA] < arrayB[positionB] ? arrayA[positionA++] : arrayB[positionB++];
+
+        while (positionA < arrayA.length) result[i++] = arrayA[positionA++];
+
+        while (positionB < arrayB.length) result[i++] = arrayB[positionB++];
+
+        return result;
+    }
+
+    public int binarySearch(int[] array, int target) throws RuntimeException {
+        int pos = Arrays.binarySearch(array, target);
+
+        if (pos >= 0) {
+            return pos;
+        }
+
+        throw new RuntimeException(String.format("Element \"%d\" not found in %s.", target, Arrays.toString(array)));
+    }
+}
---
+++ src/app/Main.java
@@ -0,0 +1,23 @@
+package app;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayUtils arrayUtils = new ArrayUtils();
+        int[] unsortedArray = new DataProvider().getData();
+        int[] sortedArray = arrayUtils.mergeSort(unsortedArray);
+
+        System.out.println("Unsorted array: " + Arrays.toString(unsortedArray));
+        System.out.println("Sorted array: " + Arrays.toString(sortedArray));
+
+        try {
+            int position = arrayUtils.binarySearch(sortedArray, 7);
+            System.out.printf("Search element (%d) in %s: position %d, value %d%n", 7, Arrays.toString(sortedArray), position, sortedArray[position]);
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+}
